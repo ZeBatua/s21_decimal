72eef9cd94c296baa8487470d8d4731005eeb69f
provemen <provemen@student.21-school.ru>
2022-08-23 15:22:02 +0300

    equal or greater

--- src/tests/test.c
+++ src/tests/test.c
@@ -6,6 +6,10 @@
 #include <float.h>
 
 
+// Возвращаемое значение - код ошибки:
+//  - 0 - OK
+//  - 1 - ошибка конвертации
+
 #define S21_EPS 1e-9
 
 // TEST FOR is_equal
@@ -125,42 +129,83 @@ START_TEST(test_is_equal5) {
                      0);
 } END_TEST
 
-// START_TEST() {
-//     s21_decimal dec1;
-//     init_decimal(&dec1);
-//     dec1.bits[3] = 0;
-//     dec1.bits[2] = 0;
-//     dec1.bits[1] = 0;
-//     dec1.bits[0] = 12594141;
+START_TEST(test_is_greater_or_equal1) {
+    s21_decimal dec1;
+    init_decimal(&dec1);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 1;
+    dec1.bits[1] = 1;
+    dec1.bits[0] = 1;
 
-//     s21_decimal dec2;
-//     init_decimal(&dec2);
-//     dec1.bits[3] = 0;
-//     dec1.bits[2] = 0;
-//     dec1.bits[1] = 0;
-//     dec1.bits[0] = 12594141;
+    s21_decimal dec2;
+    init_decimal(&dec2);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 1;
+    dec1.bits[1] = 1;
+    dec1.bits[0] = 1;
 
-//     ck_assert_int_eq(s21_is_equal(dec1, dec2),
-//                      0);
-// } END_TEST
+    ck_assert_int_eq(s21_is_greater_or_equal(dec1, dec2),
+                     0);
+} END_TEST
 
-// START_TEST(acos_6) {
-//   double num = NAN;
-//   long double orig_res = s21_acos(num), our_res = acos(num);
-//   int suc = 0;
-//   if (isnan(orig_res) && isnan(our_res)) suc = 1;
-//   ck_assert_int_eq(1, suc);
-// } END_TEST
+START_TEST(test_is_greater_or_equal2) {
+ s21_decimal dec1;
+    init_decimal(&dec1);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 0;
+    dec1.bits[1] = 888888;
+    dec1.bits[0] = 12594199;
 
-// START_TEST(acos_7) {
-//     ck_assert_ldouble_nan(s21_acos(NAN));
-//     ck_assert_ldouble_nan(acos(NAN));
-// } END_TEST
+    s21_decimal dec2;
+    init_decimal(&dec2);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 0;
+    dec1.bits[1] = 0;
+    dec1.bits[0] = 12594141;
 
-// START_TEST(acos_8) {
-//     ck_assert_ldouble_nan(s21_acos(NAN));
-//     ck_assert_ldouble_nan(acos(NAN));
-// } END_TEST
+    ck_assert_int_eq(s21_is_greater_or_equal(dec1, dec2),
+                     0);
+} END_TEST
+
+START_TEST(test_is_greater_or_equal3) {
+    s21_decimal dec1;
+    init_decimal(&dec1);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 2;
+    dec1.bits[1] = 0;
+    dec1.bits[0] = 12594199;
+
+    s21_decimal dec2;
+    init_decimal(&dec2);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 0;
+    dec1.bits[1] = 0;
+    dec1.bits[0] = 12594141;
+
+    ck_assert_int_eq(s21_is_greater_or_equal(dec1, dec2),
+                     0);
+} END_TEST
+
+START_TEST(test_is_greater_or_equal4) {
+    s21_decimal dec1;
+    init_decimal(&dec1);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 0;
+    dec1.bits[1] = 0;
+    dec1.bits[0] = 12594199;
+
+    setSign(&dec1, 1);
+
+    s21_decimal dec2;
+    init_decimal(&dec2);
+    dec1.bits[3] = 0;
+    dec1.bits[2] = 0;
+    dec1.bits[1] = 0;
+    dec1.bits[0] = 12594141;
+
+    ck_assert_int_eq(s21_is_greater_or_equal(dec1, dec2),
+                     1);
+} END_TEST
 
 // START_TEST(acos_9) {
 //     ck_assert_ldouble_eq_tol(s21_acos(1.0), acos(1.0), S21_EPS);
@@ -197,9 +242,10 @@ int main() {
     tcase_add_test(tc1_1, test_is_equal3);
     tcase_add_test(tc1_1, test_is_equal4);
     tcase_add_test(tc1_1, test_is_equal5);
-    // tcase_add_test(tc1_1, test_is_equal);
-    // tcase_add_test(tc1_1, test_is_equal1);
-    // tcase_add_test(tc1_1, test_is_equal1);
+    tcase_add_test(tc1_1, test_is_greater_or_equal1);
+    tcase_add_test(tc1_1, test_is_greater_or_equal2);
+    tcase_add_test(tc1_1, test_is_greater_or_equal3);
+    tcase_add_test(tc1_1, test_is_greater_or_equal4);
 
     srunner_run_all(sr, CK_NORMAL);
     nf = srunner_ntests_failed(sr);
