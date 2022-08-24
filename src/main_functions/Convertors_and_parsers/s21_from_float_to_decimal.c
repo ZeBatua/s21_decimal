5487b63a4f1114fc5428bf4cd5a04e13c8019b4f
Thalassa Plack <thalassa@il-j3.msk.21-school.ru>
2022-08-24 19:27:57 +0300

    fix_equal

--- src/main_functions/Convertors_and_parsers/s21_from_float_to_decimal.c
+++ src/main_functions/Convertors_and_parsers/s21_from_float_to_decimal.c
@@ -7,9 +7,8 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {
         if (binary_exp > 95) {
             return 1;
         } else {
-            for (; !((int)src), src *= 10; scale++) { // нормализовали 0.0001234 -> 1,234
-                
-            }
+            for (; !((int)src), src *= 10; scale++) {} // нормализовали 0.000123456789 -> 1,23456789
+            for (int i = 0; src *= 10, i < 8 ; scale++) {} // 8 знаков 1,23456789 -> 12345678,9
         }
     } else {
         return 1;
