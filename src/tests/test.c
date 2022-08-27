5d02770013be3dc340e4562c959c091b42fe7203
Provement Toffee <provemen@at-r4.msk.21-school.ru>
2022-08-27 20:37:19 +0300

    tests for add added

--- src/tests/test.c
+++ src/tests/test.c
@@ -939,11 +939,6 @@ START_TEST(test_add4) {
     printf("\n\n");
     smart_print_binary_decimal(dec2);
     printf("\n\n");
-    // smart_print_binary_decimal(dec2);
-    // printf("\n\n");
-    // smart_print_binary_decimal(result1);
-    // printf("\n\n");
-    // smart_print_binary_decimal(my_result);
 
 
     ck_assert_int_eq(eq_add, 1);
@@ -1039,6 +1034,56 @@ START_TEST(test_add6) {
     ck_assert_int_eq(eq_add, 1);
     
 } END_TEST
+
+
+START_TEST(test_add7) {
+
+    s21_decimal dec1;
+    init_decimal(&dec1);
+    dec1.bits[3] = 2147483648;
+    dec1.bits[2] = 4294967295;
+    dec1.bits[1] = 0;
+    dec1.bits[0] = 0;
+
+    s21_decimal dec2;
+    init_decimal(&dec2);
+    dec2.bits[3] = 0;
+    dec2.bits[2] = 0;
+    dec2.bits[1] = 4294967295;
+    dec2.bits[0] = 0;
+
+    s21_decimal my_result;
+    init_decimal(&my_result);
+    my_result.bits[3] = 2147483648;
+    my_result.bits[2] = 4294967294;
+    my_result.bits[1] = 1;
+    my_result.bits[0] = 0;
+
+    s21_decimal result1;
+
+    // 11111111111111111111111111111110 00000000000000000000000000000001 00000000000000000000000000000000
+
+    s21_add(dec1, dec2, &result1);
+
+    int eq_add = s21_is_equal(result1, my_result);
+
+    printf("\n\n\n\n\n\n");
+    smart_print_binary_decimal(dec1);
+    printf("\n\n");
+    smart_print_binary_decimal(dec2);
+    printf("\n\n");
+    smart_print_binary_decimal(result1);
+    printf("\n\n");
+    smart_print_binary_decimal(my_result);
+
+
+    ck_assert_int_eq(eq_add, 1);
+    
+} END_TEST
+
+// Добавить скейл в сложение
+
+
 // THIS MAIN FUNCTION
 
 
@@ -1109,6 +1154,7 @@ int main() {
     // tcase_add_test(tc1_1, test_add4); test for INF
     tcase_add_test(tc1_1, test_add5);
     tcase_add_test(tc1_1, test_add6);
+    tcase_add_test(tc1_1, test_add7);
 
     srunner_run_all(sr, CK_NORMAL);
     nf = srunner_ntests_failed(sr);
