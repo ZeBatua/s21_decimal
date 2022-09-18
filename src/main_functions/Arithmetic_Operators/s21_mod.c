4ef91394cd9377681af866488db99ca883d03442
Provement Toffee <provemen@ga-p6.msk.21-school.ru>
2022-09-18 19:56:33 +0300

    mod not complete

--- src/main_functions/Arithmetic_Operators/s21_mod.c
+++ src/main_functions/Arithmetic_Operators/s21_mod.c
@@ -3,8 +3,8 @@
 int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     s21_extended_decimal ext_value_1, ext_value_2, ext_result;
     int error = 0;
-    int first_extended_bit = 0;
-    int second_extended_bit = 0;
+    int dec1_non_zero_extended_bit = 0;
+    int dec2_non_zero_extended_bit = 0;
     int step_to_shift = 0;
     int width_dec1 = 0, width_dec2 = 0;
     
@@ -17,35 +17,42 @@ int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     equate_dec_to_extdec(*result, &ext_result);
     equate_ext_scale(&ext_value_1, &ext_value_2);
     // int dec1_sign = getExtSign(value_1), dec2_sign = getExtSign(value_2);
-
-    first_extended_bit = get_first_non_zero_extBit(ext_value_1);
-    second_extended_bit = get_first_non_zero_extBit(ext_value_2);
+    smart_print_exdec(ext_result);
     
-    while (width_dec1 >= width_dec2) {
-        width_dec1 = get_first_non_zero_extBit(ext_result);
-        width_dec2 = get_first_non_zero_extBit(ext_value_2);
-        if (first_extended_bit == second_extended_bit) {
-            subtraction_no_sign(ext_value_1, ext_value_2, &ext_result);
-            first_extended_bit = get_first_non_zero_extBit(ext_result);
-            // continue;
-            printf("\nzdes'\n\nzdes'\n\nzdes'\n\nzdes'\n\nzdes'\n\nzdes'\n\nzdes'\n");
-        step_to_shift = first_extended_bit - second_extended_bit;
-        // smart_print_exdec(ext_value_2);
-        shift_left(&ext_value_2, step_to_shift);
-        } else if (is_less_no_sign(ext_value_1, ext_value_2)) {
-            shift_right(&ext_value_2, 1);
-            subtraction_no_sign(ext_value_1, ext_value_2, &ext_result);
-            printf("after sub delimoe/delitel\n");
+    dec1_non_zero_extended_bit = get_first_non_zero_extBit(ext_value_1);
+    dec2_non_zero_extended_bit = get_first_non_zero_extBit(ext_value_2);
+    while (width_dec1 >= width_dec2) { // сравнение ширины делимого и делителя
+        printf("%d - width dec1\n", width_dec1);
+        printf("%d - width dec2\n", width_dec2);
+        if (!is_less_no_sign(ext_value_1, ext_value_2) || is_equal_no_sign(ext_value_1, ext_value_2) || // если делимое больше делителя
+            !is_less_no_sign(ext_result, ext_value_2) || is_equal_no_sign(ext_result, ext_value_2)) {
+            step_to_shift = dec1_non_zero_extended_bit - dec2_non_zero_extended_bit;
+            printf("step_to_shift %d\n", step_to_shift);
+            shift_left(&ext_value_2, step_to_shift);
+            subtraction_no_sign(ext_value_1, ext_value_2, &ext_value_1);
+            printf("\nzdes1\n");
+            dec1_non_zero_extended_bit = get_first_non_zero_extBit(ext_result);
+            printf("print get_first_non_zero_extBit %d\n", dec1_non_zero_extended_bit);
+            dec2_non_zero_extended_bit = get_first_non_zero_extBit(ext_value_2);
+            printf("print get_first_non_zero_extBit %d\n", dec2_non_zero_extended_bit);
             smart_print_exdec(ext_result);
+            
+        }
+        if (is_less_no_sign(ext_value_1, ext_value_2) ) { // если делимое меньше делителя
+            step_to_shift = get_first_non_zero_extBit(ext_value_2) - get_first_non_zero_extBit(ext_result);
+            shift_right(&ext_value_2, step_to_shift);
+            subtraction_no_sign(ext_value_1, ext_value_2, &ext_value_1);
+            printf("after sub delimoe/delitel\n");
+            smart_print_exdec(ext_value_1);
             printf("\nzdes'\n");
         }
         printf("\nzdes' 112121221\n");
-        subtraction_no_sign(ext_value_1, ext_value_2, &ext_result);
+        smart_print_exdec(ext_result);
         smart_print_exdec(ext_value_2);
-    
+        width_dec1 = get_first_non_zero_extBit(ext_result) - 95;
+        width_dec2 = get_first_non_zero_extBit(ext_value_2) - 95;
+        printf("%d - width dec1\n", width_dec1);
     }
-
-
     error = equate_extdec_to_dec(ext_result, result);
     return error;
 }
