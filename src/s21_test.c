45fb05996fc9911f4a657f151f2b4a3ce77220c3
Morfin Powder <morfinpo@ga-o5.msk.21-school.ru>
2022-09-21 18:00:48 +0300

    commit

--- src/s21_test.c
+++ src/s21_test.c
@@ -3404,69 +3404,69 @@ END_TEST
 
 // div
 
-// START_TEST(div_test_1) {
-//   int num1 = 100;
-//   int num2 = 50;
-//   int res_origin = 2;
-//   s21_decimal a = {0};
-//   s21_decimal b = {0};
-//   s21_from_int_to_decimal(num1, &a);
-//   s21_from_int_to_decimal(num2, &b);
-//   s21_decimal res_dec = {0};
-//   int res_int = 0;
-//   s21_div(a, b, &res_dec);
-//   s21_from_decimal_to_int(res_dec, &res_int);
-//   ck_assert_int_eq(res_int, res_origin);
-// }
-// END_TEST
+START_TEST(div_test_1) {
+  int num1 = 100;
+  int num2 = 50;
+  int res_origin = 2;
+  s21_decimal a = {0};
+  s21_decimal b = {0};
+  s21_from_int_to_decimal(num1, &a);
+  s21_from_int_to_decimal(num2, &b);
+  s21_decimal res_dec = {0};
+  int res_int = 0;
+  s21_div(a, b, &res_dec);
+  s21_from_decimal_to_int(res_dec, &res_int);
+  ck_assert_int_eq(res_int, res_origin);
+}
+END_TEST
 
-// START_TEST(div_test_2) {
-//   int num1 = -32768;
-//   int num2 = 2;
-//   int res_origin = -16384;
-//   s21_decimal a = {0};
-//   s21_decimal b = {0};
-//   s21_from_int_to_decimal(num1, &a);
-//   s21_from_int_to_decimal(num2, &b);
-//   s21_decimal res_dec = {0};
-//   int res_int = 0;
-//   s21_div(a, b, &res_dec);
-//   s21_from_decimal_to_int(res_dec, &res_int);
-//   ck_assert_int_eq(res_int, res_origin);
-// }
-// END_TEST
+START_TEST(div_test_2) {
+  int num1 = -32768;
+  int num2 = 2;
+  int res_origin = -16384;
+  s21_decimal a = {0};
+  s21_decimal b = {0};
+  s21_from_int_to_decimal(num1, &a);
+  s21_from_int_to_decimal(num2, &b);
+  s21_decimal res_dec = {0};
+  int res_int = 0;
+  s21_div(a, b, &res_dec);
+  s21_from_decimal_to_int(res_dec, &res_int);
+  ck_assert_int_eq(res_int, res_origin);
+}
+END_TEST
 
-// START_TEST(div_test_3) {
-//   int num1 = 2;
-//   int num2 = 2;
-//   int res_origin = 1;
-//   s21_decimal a = {0};
-//   s21_decimal b = {0};
-//   s21_from_int_to_decimal(num1, &a);
-//   s21_from_int_to_decimal(num2, &b);
-//   s21_decimal res_dec = {0};
-//   int res_int = 0;
-//   s21_div(a, b, &res_dec);
-//   s21_from_decimal_to_int(res_dec, &res_int);
-//   ck_assert_int_eq(res_int, res_origin);
-// }
-// END_TEST
+START_TEST(div_test_3) {
+  int num1 = 2;
+  int num2 = 2;
+  int res_origin = 1;
+  s21_decimal a = {0};
+  s21_decimal b = {0};
+  s21_from_int_to_decimal(num1, &a);
+  s21_from_int_to_decimal(num2, &b);
+  s21_decimal res_dec = {0};
+  int res_int = 0;
+  s21_div(a, b, &res_dec);
+  s21_from_decimal_to_int(res_dec, &res_int);
+  ck_assert_int_eq(res_int, res_origin);
+}
+END_TEST
 
-// START_TEST(div_test_4) {
-//   int num1 = 0;
-//   int num2 = 5;
-//   int res_origin = num1 / num2;
-//   s21_decimal a = {0};
-//   s21_decimal b = {0};
-//   s21_from_int_to_decimal(num1, &a);
-//   s21_from_int_to_decimal(num2, &b);
-//   s21_decimal res_dec = {0};
-//   int res_int = 0;
-//   s21_div(a, b, &res_dec);
-//   s21_from_decimal_to_int(res_dec, &res_int);
-//   ck_assert_int_eq(res_int, res_origin);
-// }
-// END_TEST
+START_TEST(div_test_4) {
+  int num1 = 0;
+  int num2 = 5;
+  int res_origin = num1 / num2;
+  s21_decimal a = {0};
+  s21_decimal b = {0};
+  s21_from_int_to_decimal(num1, &a);
+  s21_from_int_to_decimal(num2, &b);
+  s21_decimal res_dec = {0};
+  int res_int = 0;
+  s21_div(a, b, &res_dec);
+  s21_from_decimal_to_int(res_dec, &res_int);
+  ck_assert_int_eq(res_int, res_origin);
+}
+END_TEST
 
 // equal
 
@@ -10103,10 +10103,10 @@ int main() {
 
   // div
 
-  // tcase_add_test(tc, div_test_1);
-  // tcase_add_test(tc, div_test_2);
-  // tcase_add_test(tc, div_test_3);
-  // tcase_add_test(tc, div_test_4);
+  tcase_add_test(tc, div_test_1);
+  tcase_add_test(tc, div_test_2);
+  tcase_add_test(tc, div_test_3);
+  tcase_add_test(tc, div_test_4);
 
   // equal
 
