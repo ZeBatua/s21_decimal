ee0edbe4b5908ba6a02f6913d42f3dcb50076876
Thalassa Plack <thalassa@mi-g5.msk.21-school.ru>
2022-10-04 14:36:05 +0300

    style_script

---
+++ style_script.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+clang-format -n src/extended_decimal/*.c
+clang-format -n src/main_functions/Another_functions/*.c
+clang-format -n src/main_functions/Arithmetic_Operators/*.c
+clang-format -n src/main_functions/Comparison_Operators/*.c
+clang-format -n src/main_functions/Convertors_and_parsers/*.c
+clang-format -n src/secondary_functions/Bit_operations/*.c
+clang-format -n src/secondary_functions/convert_dec/*.c
+clang-format -n src/secondary_functions/float_functions/*.c
+clang-format -n src/secondary_functions/GetSet_SignScale/*.c
+clang-format -n src/secondary_functions/NoSE/*.c
+clang-format -n src/secondary_functions/shifts/*.c
