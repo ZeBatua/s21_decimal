c5d824465dc2c86767e180e21e42e794e6c243bb
Thalassa Plack <thalassa@mi-g5.msk.21-school.ru>
2022-10-04 14:37:02 +0300

    reset_materials

--- materials/build/Valgrind/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-make s21_decimal.a
-gcc -g s21_test.c s21_decimal.a -o test -lcheck -lcheck -lpthread -lm -D_GNU_SOURCE -lrt -lsubunit
-valgrind --trace-children=yes --track-fds=yes --track-origins=yes --leak-check=full --show-leak-kinds=all ./test
